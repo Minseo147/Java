c745936dcb04a52c8e0f91da442e176ff8147727
Minseo147 <113977304+Minseo147@users.noreply.github.com>
2022-10-05 20:55:14 +0900

    Add files via upload

---
+++ MiniProject.java
@@ -0,0 +1,12 @@
+
+public class MiniProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(10+20);
+		System.out.println(20-10);
+		System.out.println(10*20);
+		System.out.println(20/10);
+	}
+
+}
