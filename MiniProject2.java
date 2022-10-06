5f5e9f55f18ed102424ffa3b311befb9c570b0f7
Minseo147 <113977304+Minseo147@users.noreply.github.com>
2022-10-06 15:31:05 +0900

    Add files via upload

---
+++ MiniProject2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class MiniProject2 {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		double F, C; //화씨, 섭씨
+		
+		System.out.println("==========================");
+		System.out.println("1. 화씨->섭씨 ");
+		System.out.println("2. 섭씨->화씨 ");
+		System.out.println("==========================");
+		System.out.print("번호를 선택하시오: ");
+		int x = sc.nextInt(); //menu 선택
+		
+		if (x==1) { //화씨 -> 섭씨
+			System.out.print("화씨온도를 입력하시오: ");
+			F = sc.nextDouble();
+			C = (F-32)/1.8;
+			System.out.println("섭씨온도는 " + C);
+		}
+		else if (x==2) { //섭씨 -> 화씨
+			System.out.print("섭씨온도를 입력하시오: ");
+			C = sc.nextDouble();
+			F = (C*1.8) + 32;
+			System.out.println("화씨온도는 " + F);
+		}
+		
+		
+	}
+
+}
